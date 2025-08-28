13dd9632ef73ce6988b4e687a7c42f0d18ab1365
AYOMIFROSH <rhymeanabel@gmail.com>
2025-08-28 18:27:06 +0100

    src/pages/BuyNumber.tsx

--- smsverification/src/pages/BuyNumber.tsx
+++ smsverification/src/pages/BuyNumber.tsx
@@ -274,7 +274,7 @@ const BuyNumber: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50 lg:bg-transparent">
       {/* Mobile Header */}
-      <div className="lg:hidden bg-white border-b border-gray-200 sticky top-16 z-30">
+      <div className="lg:hidden bg-white border-b border-gray-200 sticky top-0 z-20">
         <div className="px-4 py-3">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-3">
