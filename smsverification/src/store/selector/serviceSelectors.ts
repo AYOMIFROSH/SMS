bc46af6ea14c8782609a532e3a8caed364fd9fc5
AYOMIFROSH <rhymeanabel@gmail.com>
2025-12-19 23:17:53 +0100

    operator fixed

--- smsverification/src/store/selector/serviceSelectors.ts
+++ smsverification/src/store/selector/serviceSelectors.ts
@@ -1,10 +1,40 @@
 // src/store/selectors/servicesSelectors.ts - FIXED: All TypeScript errors
 import { RootState } from '@/store/store';
 
+// Selector to get current selection data
+// export const getCurrentSelectionData = (state: RootState) => {
+//   const { services, countries, operators, selectedCountry, selectedService, selectedOperator } = state.services;
+//   
+//   return {
+//     country: selectedCountry ? countries.find(c => c.code === selectedCountry) : null,
+//     service: selectedService ? services.find(s => s.code === selectedService) : null,
+//     operator: selectedCountry && selectedOperator 
+//       ? operators[selectedCountry]?.find(o => o.id === selectedOperator) 
+//       : null
+//   };
+// };
 
-export default function OperatorSelector() {
-  return null;
-}
+// Other useful selectors
+export const getSelectedCountry = (state: RootState) => {
+  const { countries, selectedCountry } = state.services;
+  return selectedCountry ? countries.find(c => c.code === selectedCountry) : null;
+};
+
+export const getSelectedService = (state: RootState) => {
+  const { services, selectedService } = state.services;
+  return selectedService ? services.find(s => s.code === selectedService) : null;
+};
+
+// export const getSelectedOperator = (state: RootState) => {
+//   const { operators, selectedCountry, selectedOperator } = state.services;
+//   return selectedCountry && selectedOperator 
+//     ? operators[selectedCountry]?.find(o => o.id === selectedOperator) 
+//     : null;
+// };
+
+// export const getOperatorsForCountry = (state: RootState, country: string) => {
+//   return state.services.operators[country] || [];
+// };
 
 export const getPriceForService = (state: RootState, country: string, service: string, operator?: string) => {
   const { prices } = state.services;
